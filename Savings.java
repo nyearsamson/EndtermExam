572827ddd367c9d0d171149c889deff8ac44c107
nyearsamson <79901487+nyearsamson@users.noreply.github.com>
2021-05-18 14:35:04 +0800

    Update Savings.java

--- Savings.java
+++ Savings.java
@@ -1,40 +1,31 @@
-public class Savings extends Account
-{
-	private double interestRate;
-	
-	public Savings(int accountNumber, double interest)
-	{
-		super(accountNumber);
-		setInterestRate(interest);
-	}
-	
-	public void setInterestRate(double interest)
-	{
-		interestRate = interest;
-	}
-	public double getInterestRate()
-	{
-		return interestRate;
-	}
-	
-	public int getAccountNumber()
-	{
-		return accountNumber;
-	}
-	
-	public double getBalance()
-	{
-		return balance;
-	}
-	
-	/*
-	 * I was unsure what it meant by "[...] the get method displays the String
-	 * [...]" so I assumed that putting that in the two get methods made no sense
-	 * and opted to do the following. In this situation I feel like the following
-	 * could be added to the abstract as an empty method to force its creation.
-	 */
-	public String getAccountInfo()
-	{
-		return("Savings Account Information\nAccount Number: " + getAccountNumber() + "\nBalance: " + getBalance() + "\nInterest Rate: " + getInterestRate() + "%");
-	}
+public class Savings extends Account{
+
+   private double interestRate;
+
+   public Savings(int accNo, double rate){
+
+       super(accNo);
+
+       this.interestRate = rate;
+
+   }
+
+   public double getInterestRate() {
+
+       return interestRate;
+
+   }
+
+   public void setInterestRate(double interestRate) {
+
+       this.interestRate = interestRate;
+
+   }
+
+   public String getAccountDetails(){
+
+       return "Savings Account Information\nAccount No: "+getAccountNo()+"\nAccount Balance: "+getAccountBalance()+"\nInterest Rate: "+getInterestRate();
+
+   }
+
 }
