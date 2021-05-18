34812c7f098f83397c9dcc36b30d43cfaa0b90ce
nyearsamson <79901487+nyearsamson@users.noreply.github.com>
2021-05-18 14:34:11 +0800

    Update Checking.java

--- Checking.java
+++ Checking.java
@@ -1,28 +1,11 @@
-public class Checking extends Account
-{
-	public Checking(int accountNumber)
-	{
-		super(accountNumber);
-	}
-	
-	public int getAccountNumber()
-	{
-		return accountNumber;
-	}
-	
-	public double getBalance()
-	{
-		return balance;
-	}
-	
-	/*
-	 * I was unsure what it meant by "[...] the get method displays the String
-	 * [...]" so I assumed that putting that in the two get methods made no sense
-	 * and opted to do the following. In this situation I feel like the following
-	 * could be added to the abstract as an empty method to force its creation.
-	 */
-	public String getAccountInfo()
-	{
-		return("Checking Account Information\nAcct Num: " + getAccountNumber() + "\nBalance: " + getBalance());
-	}
-}
+public class Checking extends Account{
+
+   public Checking(int accNO){
+
+       super(accNO);
+   }
+   public String getAccountDetails()
+   {
+    return "Checkings Account Information\nAccount No: "+getAccountNo()+"\nAccount Balance: "+getAccountBalance();
+   }
+   }
